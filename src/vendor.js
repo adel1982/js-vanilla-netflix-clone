6b860bcdd75fe80d7e2aa9f22f35f6f5059a9fb4
Adel Kamel <adelkamel1982@gmail.com>
2019-05-31 12:11:25 +0200

    Table integration

--- src/vendor.js
+++ src/vendor.js
@@ -1,6 +1,10 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight'
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons/faDoorOpen'
+import { faTabletAlt } from '@fortawesome/free-solid-svg-icons/faTabletAlt'
+import { faTags } from '@fortawesome/free-solid-svg-icons/faTags'
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes'
 
-library.add(faCheck, faChevronRight)
+library.add(faCheck, faChevronRight, faDoorOpen, faTabletAlt, faTags, faTimes)
 dom.watch()
