71415e7fd4fe2b174e17f70a928f61b1751353f6
Adel Kamel <adelkamel1982@gmail.com>
2019-05-31 20:57:58 +0200

    Development of tab behaviors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './scss/main.scss'
+
+const tabsItems = document.querySelectorAll('.tab-item')
+const tabsContentItems = document.querySelectorAll('.tab-content-item')
+
+tabsItems.forEach(tabsItem => tabsItem.addEventListener('click', selectItem ))
+
+function selectItem(e) {
+  removeBorder()
+  hiddenContent()
+  this.classList.add('tab-active')
+  const tabContentItem = document.querySelector(`#${this.id}-content`)
+  tabContentItem.classList.add('show')
+}
+
+function removeBorder() {
+  tabsItems.forEach(tabsItem => tabsItem.classList.remove('tab-active'))
+}
+
+function hiddenContent() {
+  tabsContentItems.forEach(tabContentItem => tabContentItem.classList.remove('show'))
+}
